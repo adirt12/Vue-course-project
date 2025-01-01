d0f702c8da99312919086a138d9f39d52c9a3369
adirt12 <adirt12.com>
2025-01-01 17:05:49 +0200

    starting new project

---
+++ src/App.vue
@@ -0,0 +1,30 @@
+<script setup>
+  import { ref } from 'vue';
+
+  const name = ref('adi levi');
+  const status = ref('active');
+  const tasks = ref(['Task One','Task Two','Task Three']);
+  
+  const toggleStatus = () => {
+    if (status.value === 'active'){
+      status.value = 'pending'
+    }
+    else if(status.value === 'pending') {
+      status.value = 'inactive'
+    }
+    else{
+      status.value = 'active'
+    }
+  }
+</script>
+
+<template>
+  <h1>{{name}}</h1>
+  <p>User is {{ status }}</p>
+  <h2>Tasks:</h2>
+  <ul>
+    <li v-for="task in tasks">{{ task }}</li>
+  </ul>
+  <button @click="toggleStatus">Change status</button>
+</template>
+
