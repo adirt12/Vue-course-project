0cb88d141168e98c70a817d6a62cf5a1feffb2b4
adirt12 <adirt12.com>
2025-01-01 18:41:18 +0200

    create cards , jobs , navbar componentes

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,15 @@
 <script setup>
-  import { ref } from 'vue';
+    import Navbar from './components/Navbar.vue';
+    import Hero from '@/components/Hero.vue'
+    import HomeCard from './components/HomeCard.vue';
+    import JobListings from './components/JobListings.vue';
 
-  const name = ref('adi levi');
-  const status = ref('active');
-  const tasks = ref(['Task One','Task Two','Task Three']);
-  
-  const toggleStatus = () => {
-    if (status.value === 'active'){
-      status.value = 'pending'
-    }
-    else if(status.value === 'pending') {
-      status.value = 'inactive'
-    }
-    else{
-      status.value = 'active'
-    }
-  }
 </script>
 
 <template>
-  <h1>{{name}}</h1>
-  <p>User is {{ status }}</p>
-  <h2>Tasks:</h2>
-  <ul>
-    <li v-for="task in tasks">{{ task }}</li>
-  </ul>
-  <button @click="toggleStatus">Change status</button>
+    <Navbar/>
+    <Hero />
+    <HomeCard/>
+    <JobListings/>
 </template>
 
